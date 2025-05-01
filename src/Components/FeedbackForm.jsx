e98833e96178d39e25752a6eb4b31c5ce39c23aa
Mao Soldevilla <maolenin@gmail.com>
2025-05-01 18:55:42 +0800

    Updated rating

--- src/Components/FeedbackForm.jsx
+++ src/Components/FeedbackForm.jsx
@@ -7,7 +7,8 @@ const FeedbackForm = () => {
   const [formData, setFormData] = useState({
     name: "",
     email: "",
-    feedback: ""
+    feedback: "",
+    rating: ""
   });
 
   const handleChange = (event) => {
@@ -24,14 +25,17 @@ const FeedbackForm = () => {
     Name: ${formData.name}
     Email: ${formData.email}
     Feedback: ${formData.feedback}
+    Rating: ${formData.rating}
     `;
+
     const isConfirmed = window.confirm(`Please confirm your details:\n\n${confirmationMessage}`);
     if (isConfirmed) {
       console.log('Submitting feedback', formData);
       setFormData({
         name: '',
         email: '',
-        feedback: ''
+        feedback: '',
+        rating: '', //rating after sumibision
       });
       alert('Thank you for your valueable feedback!');
     }
@@ -62,6 +66,55 @@ const FeedbackForm = () => {
           value={formData.feedback}
           onChange={handleChange}
         ></textarea>
+
+        <div style={{display:'flex',gap:'10px',flexDirection:'column'}}>
+          <span>Rate us:</span>
+          <p>
+            <input
+              type='radio'
+              name='rating'
+              value='1'
+              onChange={handleChange}
+              /> 1
+          </p>
+
+          <p>
+            <input
+              type='radio'
+              name='rating'
+              value='2'
+              onChange={handleChange}
+              /> 2
+          </p>
+
+          <p>
+            <input
+              type='radio'
+              name='rating'
+              value='3'
+              onChange={handleChange}
+              /> 3
+          </p>
+
+          <p>
+            <input
+              type='radio'
+              name='rating'
+              value='4'
+              onChange={handleChange}
+              /> 4
+          </p>
+
+          <p>
+            <input
+              type='radio'
+              name='rating'
+              value='5'
+              onChange={handleChange}
+              /> 5
+          </p>
+
+        </div>
 
         <button type="submit">Submit feedback</button>
 
